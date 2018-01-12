8aecfe7335fc994acbc51ec86b4018d684525370
kento-oka <kento.oka@kentoka.com>
2018-01-12 10:41:32 +0900

    例外追加

---
+++ src/Message/Exception/UnusableException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * FratilyPHP Http
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento.oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Http\Message\Exception;
+
+class UnusableException extends \RuntimeException implements HttpException{
+    
+    public function __construct(string $msg = null, int $code = 0, \Throwable $prev = null){
+        $msg    = $msg ?? "The stream is in an unusable state";
+        
+        parent::__construct($msg, $code, $prev);
+    }
+}
