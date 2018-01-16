ac7d8d1585335a56f87c3a9ea74f0b6c70b9f4ae
kento-oka <kento.oka@kentoka.com>
2018-01-16 09:08:01 +0900

    いくつかのショートカットとコンテンツタイプの操作方法を追加

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -98,6 +98,95 @@ class Response extends Message implements ResponseInterface{
     private $code;
 
     /**
+     * 
+     * 
+     * @param   int $code
+     * @param   mixed[] $headers
+     * 
+     * @return  static
+     */
+    public function emptyContents(int $code = 204, array $headers = []){
+        return (new static($code, $headers, Stream::temp("r")));
+    }
+    
+    /**
+     * 
+     * 
+     * @param   string  $body
+     * @param   int $code
+     * @param   mixed[] $headers
+     * @param   string  $charset
+     * 
+     * @return  static
+     */
+    public function text(
+        string $body = "",
+        int $code = 200,
+        array $headers = [],
+        string $charset = "utf-8"
+    ){
+        $stream = Stream::memory();
+        $stream->write($body);
+        
+        return (new static($code, $headers, $stream))
+            ->withContentType("text/plain; charset={$charset}");
+    }
+    
+    /**
+     * 
+     * 
+     * @param   string  $body
+     * @param   int $code
+     * @param   mixed[] $headers
+     * @param   string  $charset
+     * 
+     * @return  static
+     */
+    public function html(
+        string $body = "",
+        int $code = 200,
+        array $headers = [],
+        string $charset = "utf-8"
+    ){
+        $stream = Stream::memory();
+        $stream->write($body);
+        
+        return (new static($code, $headers, $stream))
+            ->withContentType("text/html; charset={$charset}");
+    }
+    
+    /**
+     * 
+     * 
+     * @param   mixed[] $data
+     * @param   int $code
+     * @param   mixed[] $headers
+     * 
+     * @return  static
+     */
+    public function json(
+        array $data = [],
+        int $code = 200,
+        array $headers = []
+    ){
+        $stream = Stream::memory();
+        
+        json_encode(null);
+        $json   = json_encode($data);
+        
+        if(json_last_error() !== JSON_ERROR_NONE){
+            throw new \RuntimeException(
+                "Json encode error: " . json_last_error_msg()
+            );
+        }
+        
+        $stream->write($json);
+        
+        return (new static($code, $headers, $stream))
+            ->withContentType("text/html; charset={$charset}");
+    }
+    
+    /**
      * Constructor
      *
      * @param   int $code
@@ -154,6 +243,29 @@ class Response extends Message implements ResponseInterface{
             $return->code   = $code;
         }
 
+        return $return;
+    }
+    
+    public function getContentType(){
+        if(!$this->hasHeader("content-type")){
+            return "";
+        }
+        
+        return $this->getHeaderLine("content-type");
+    }
+    
+    public function withContentType(string $contentType){
+        if($this->hasHeader("content-type")
+            && $this->getHeaderLine("content-type") === $contentType
+        ){
+            $return = $this;
+        }else{
+            $return = clone $this;
+            $return = $return
+                ->withoutHeader("content-type")
+                ->withHeader("Content-Type", $contentType);
+        }
+        
         return $return;
     }
 }
