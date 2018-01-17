610784ebb08c8c34323c868734067e271802f969
kento-oka <kento.oka@kentoka.com>
2018-01-17 14:14:06 +0900

    Sampleミドルウェアを追加

---
+++ src/Server/SampleMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * FratilyPHP Http
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento.oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Http\Server;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * 
+ */
+class SampleMiddleware implements MiddlewareInterface{
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface{
+        //  do something
+        //  ex) $request    = $request->withAttribute("newAttribute", "value");
+        
+        $response = $handler->handle($request);
+        
+        //  do something
+        //  ex) $response->getBody()->write(time());
+        
+        return $response;
+    }
+}
