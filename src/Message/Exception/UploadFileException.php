345356aaa50edb4a86f0ce097dd002f412136ee6
kento-oka <kento.oka@kentoka.com>
2018-01-27 20:53:27 +0900

    例外追加

---
+++ src/Message/Exception/UploadFileException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * FratilyPHP Http
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento.oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Http\Message\Exception;
+
+class UploadFileException extends \RuntimeException{
+
+    public static function moved(){
+        return new static("");
+    }
+}
