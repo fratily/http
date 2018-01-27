60808f10308d3cf74bc254f7b0d4044455390d71
kento-oka <kento.oka@kentoka.com>
2018-01-27 21:40:09 +0900

    アップロードエラーを追加

--- src/Message/Exception/UploadFileException.php
+++ src/Message/Exception/UploadFileException.php
@@ -15,6 +15,21 @@ namespace Fratily\Http\Message\Exception;
 
 class UploadFileException extends \RuntimeException{
 
+    const ERROR_MAP = [
+        UPLOAD_ERR_OK           => "",
+        UPLOAD_ERR_INI_SIZE     => "",
+        UPLOAD_ERR_FORM_SIZE    => "",
+        UPLOAD_ERR_PARTIAL      => "",
+        UPLOAD_ERR_NO_FILE      => "",
+        UPLOAD_ERR_NO_TMP_DIR   => "",
+        UPLOAD_ERR_CANT_WRITE   => "",
+        UPLOAD_ERR_EXTENSION    => "",
+    ];
+
+    public static function uploadError(int $error){
+        return new static(self::ERROR_MAP[$error] ?? "");
+    }
+
     public static function moved(){
         return new static("");
     }
